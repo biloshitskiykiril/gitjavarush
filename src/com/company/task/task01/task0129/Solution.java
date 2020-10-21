46fb22aa3980898d2ddda5587de90a1ca70a118c
biloshitskiykiril <biloshitskiykiril@gmail.com>
2020-10-21 15:19:11 +0300

    Видалено папку добавлено проек 0129

---
+++ src/com/company/task/task01/task0129/Solution.java
@@ -0,0 +1,16 @@
+package com.company.task.task01.task0129;
+
+/*
+Считаем длину окружности
+*/
+
+public class Solution {
+	public static void main(String[] args){
+		printCircleLengt(5);
+	}
+	
+	public static void printCircleLengt(int i){
+		double r = 2 * 3.14 * i;
+		System.out.println(r);
+	}
+}
